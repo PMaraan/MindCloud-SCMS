3b093f4dc271e01d54a83696c955239062e7dd5e
PMaraan <maraanpatrick@gmail.com>
2025-06-28 22:59:58 +0800

    Fix CSS images paths

--- public/login.php
+++ public/login.php
@@ -36,7 +36,7 @@ if ($user) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LPU-SCMS | Login</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="login-styles.css">
+  <link rel="stylesheet" href="assets/css/login-styles.css">
 </head>
 <body>
   <div class="container-fluid">
