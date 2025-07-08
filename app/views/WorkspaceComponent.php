330d946f3ff773c03d63e286f2eaf96e153354b6
LanceMorada <lanc3r.m0rad@gmail.com>
2025-07-08 19:34:02 +0800

    Workspace Update: Modular CSS & JS

--- app/views/WorkspaceComponent.php
+++ app/views/WorkspaceComponent.php
@@ -1,11 +1,42 @@
+<?php
+  $page = $_GET['page'] ?? 'index';
+
+  // CSS and JS mappings
+  $page_css = [
+    'templates' => '../../public/assets/css/Templates.css',
+  ];
+
+  $page_js = [
+    'templates' => 'assets/js/Templates.js'
+  ];
+
+  // Content mapping
+  $allowed_pages = [
+    'index' => 'index.php',
+    'approve' => '#',
+    'note' => '#',
+    'prepare' => '#',
+    'revise' => '#',
+    'faculty' => '#',
+    'templates' => 'Templates.php',
+    'syllabus' => '#',
+    'college' => 'College.php',
+    'secretary' => '#',
+    'courses' => '#'
+  ];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LPU-SCMS</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+
+  <?php if (isset($page_css[$page])): ?>
+    <link rel="stylesheet" href="<?= $page_css[$page] ?>">
+  <?php endif; ?>
 </head>
 <body>
 
@@ -17,22 +48,6 @@
     <div class="workspace">
       <div class="container py-5">
         <?php
-          $page = $_GET['page'] ?? 'index';
-
-          $allowed_pages = [
-            'index' => 'index.php',
-            'approve' => '#',
-            'note' => '#',
-            'prepare' => '#',
-            'revise' => '#',
-            'faculty' => '#',
-            'templates' => 'Templates.php',
-            'syllabus' => '#',
-            'college' => 'College.php',
-            'secretary' => '#',
-            'courses' => '#'
-          ];
-
           if (array_key_exists($page, $allowed_pages)) {
             include $allowed_pages[$page];
           } else {
@@ -42,6 +57,10 @@
       </div>
     </div>
   </div>
+
+  <?php if (isset($page_js[$page])): ?>
+    <script src="<?= $page_js[$page] ?>"></script>
+  <?php endif; ?>
 
 </body>
 </html>
